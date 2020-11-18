d091ecb61d03d85268e7d684f3685af0f8e12f50
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-11-18 13:00:50 +0000

    Mentors table Created and insert some data

---
+++ week-1/mandatory/2-classes-db/result.sql
@@ -0,0 +1,22 @@
+--1--
+createdb cyf_classes;
+
+--2--
+CREATE TABLE mentors (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  years INT NOT NULL,
+  address VARCHAR(120) NOT NULL,
+  favourite_language VARCHAR(30)
+);
+
+--3--
+INSERT INTO mentors (name, years, address, favourite_language)
+  VALUES
+    ('Wahab Rahman', 22, '123 New Street, Birmingham', 'React'),
+    ('Emile Pawfard', 22, '123 White House', 'TypeScript'),
+    ('Nick Todsman ', 54, 'Capgemini, Telford', 'Python'),
+    ('Shukri', 22, '123 Moor Street', 'Java'),
+    ('Atanas', 27, '123 Creative Street', 'Javascript');
+
+--4--
