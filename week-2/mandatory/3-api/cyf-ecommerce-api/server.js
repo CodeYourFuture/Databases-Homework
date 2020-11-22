866967c33f642d120867a2afe9f33b8db51f3265
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-11-22 17:38:25 +0000

    week-2 homework is done

--- week-2/mandatory/3-api/cyf-ecommerce-api/server.js
+++ week-2/mandatory/3-api/cyf-ecommerce-api/server.js
@@ -17,6 +17,18 @@ app.get("/customers", function(req, res) {
     });
 });
 
+app.get("/suppliers", function(req, res) {
+    pool.query('SELECT * FROM suppliers', (error, result) => {
+        res.json(result.rows);
+    });
+});
+
+app.get("/products", function(req, res) {
+    pool.query('SELECT products.product_name, suppliers.supplier_name FROM products INNER JOIN suppliers ON products.supplier_id = suppliers.id', (error, result) => {
+        res.json(result.rows);
+    });
+});
+
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
 });
