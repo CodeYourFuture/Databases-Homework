964db5cc9733394c16f3084c0a217173f643d689
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-11-28 15:49:48 +0000

    class ex 4 is done

--- week-3/cyf_hotels_api/server.js
+++ week-3/cyf_hotels_api/server.js
@@ -12,9 +12,37 @@ const pool = new Pool({
   port: 5432,
 });
 
+app.get("/", function (req, res) {
+ 
+res.send('This is Hotel CYF. Wellcomme!');
+});
+
 app.get("/hotels", function (req, res) {
   pool
-    .query("SELECT * FROM hotels")
+    .query("SELECT * FROM hotels ORDER by name")
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get("/hotels/:hotelId", function (req, res) {
+  const hotelId = req.params.hotelId;
+
+  pool
+    .query("SELECT * FROM hotels WHERE id=$1", [hotelId])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get("/hotels", function (req, res) {
+  const hotelNameQuery = req.query.name;
+  let query = `SELECT * FROM hotels ORDER BY name`;
+
+  if (hotelNameQuery) {
+    query = `SELECT * FROM hotels WHERE name LIKE '%${hotelNameQuery}%' ORDER BY name`;
+  }
+
+  pool
+    .query(query)
     .then((result) => res.json(result.rows))
     .catch((e) => console.error(e));
 });
@@ -29,7 +57,7 @@ app.post("/hotels", function (req, res) {
       .status(400)
       .send("The number of rooms should be a positive integer.");
   }
-
+  
   pool
     .query("SELECT * FROM hotels WHERE name=$1", [newHotelName])
     .then((result) => {
@@ -49,6 +77,133 @@ app.post("/hotels", function (req, res) {
 });
 
 
+app.post("/customers", function (req, res) {
+  const newCustomerName = req.body.name;
+  const newCustomerEmail = req.body.email;
+  const newCustomerAddress = req.body.address;
+  const newCustomerCity = req.body.city;
+  const newCustomerPostcode = req.body.postcode;
+  const newCustomerCountry = req.body.country;
+
+  pool
+  .query("SELECT * FROM customers WHERE name=$1", [newCustomerName])
+  .then((result) => {
+      if (result.rows.length > 0) {
+        return res
+          .status(400)
+          .send("A customer with the same name already exists!");
+      } else {
+        const query =
+          "INSERT INTO customers (name, email, address, city, postcode, country) VALUES ($1, $2, $3, $4, $5, $6)";
+        pool
+          .query(query, [newCustomerName, newCustomerEmail, newCustomerAddress, newCustomerCity, newCustomerPostcode, newCustomerCountry])
+          .then(() => res.send("Customer created!"))
+          .catch((e) => console.error(e));
+      }
+    });
+});
+
+app.get ('/customers', function (req, res){
+      pool
+    .query("SELECT * FROM customers ORDER by name")
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+
+});
+
+app.get("/customers/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+
+  pool
+    .query("SELECT * FROM customers WHERE id=$1", [customerId])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+
+app.get("/customers/:customerId/bookings", function (req, res) {
+  const customerId = req.params.customerId;
+
+  pool
+    .query("SELECT * FROM bookings WHERE customer_id = $1", [customerId])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+
+app.put("/customers/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+
+  const newEmail = req.body.email;
+  const newAddress = req.body.address;
+  const newCity = req.body.city;
+  const newPostcode = req.body.postcode;
+  const newCountry = req.body.country; 
+
+
+  if(newCity){
+    pool
+    .query("UPDATE customers SET city=$1 WHERE id=$2", [newCity, customerId])
+    .then(() => res.send(`Customer ${customerId} updated!`))
+    .catch((e) => console.error(e));
+  } else if(newAddress){
+    pool
+    .query("UPDATE customers SET address=$1 WHERE id=$2", [newAddress, customerId])
+    .then(() => res.send(`Customer ${customerId} updated!`))
+    .catch((e) => console.error(e));
+  } else if(newPostcode){
+    pool
+    .query("UPDATE customers SET postcode=$1 WHERE id=$2", [newPostcode, customerId])
+    .then(() => res.send(`Customer ${customerId} updated!`))
+    .catch((e) => console.error(e));
+  }else if(newCountry){
+    pool
+    .query("UPDATE customers SET country=$1 WHERE id=$2", [newCountry, customerId])
+    .then(() => res.send(`Customer ${customerId} updated!`))
+    .catch((e) => console.error(e));
+  }else if(newEmail){
+    pool
+    .query("UPDATE customers SET email=$1 WHERE id=$2", [newEmail, customerId])
+    .then(() => res.send(`Customer ${customerId} updated!`))
+    .catch((e) => console.error(e));
+  } else{
+      return res
+          .status(400)
+          .send("Please put your email address.");
+  }
+
+});
+
+app.delete("/customers/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+
+  pool
+    .query("DELETE FROM bookings WHERE customer_id=$1", [customerId])
+    .then(() => {
+      pool
+        .query("DELETE FROM customers WHERE id=$1", [customerId])
+        .then(() => res.send(`Customer ${customerId} deleted!`))
+        .catch((e) => console.error(e));
+    })
+    .catch((e) => console.error(e));
+});
+
+
+app.delete ('/hotels/:hotelId', function (req,res){
+    const hotelId = req.params.hotelId;
+    
+    pool
+    .query("DELETE FROM bookings WHERE hotel_id=$1", [hotelId])
+    .then(() => {
+      pool
+        .query("DELETE FROM hotels WHERE id=$1", [hotelId])
+        .then(() => res.send(`Hotel ${hotelId} deleted!`))
+        .catch((e) => console.error(e));
+    })
+    .catch((e) => console.error(e));
+});
+
 app.listen(3000, function () {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
+  console.log("Server is listening on port 3000. Ready to accept requests!")
+
 });
